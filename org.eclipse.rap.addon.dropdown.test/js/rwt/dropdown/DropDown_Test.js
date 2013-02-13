cf32c9fa505658d8f6717b4f3eed4d38dd223d3c
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-02-13 13:09:11 +0100

    Minimal client side implementation

--- org.eclipse.rap.addon.dropdown.test/js/rwt/dropdown/DropDown_Test.js
+++ org.eclipse.rap.addon.dropdown.test/js/rwt/dropdown/DropDown_Test.js
@@ -14,14 +14,103 @@
 
 var TestUtil = org.eclipse.rwt.test.fixture.TestUtil;
 
+var shell;
+var widget;
+var dropdown;
+var popup;
+
 rwt.qx.Class.define( "rwt.dropdown.DropDown_Test", {
 
   extend : rwt.qx.Object,
 
   members : {
 
-    testCreateInstance : function() {
-      new rwt.dropdown.DropDown();
+    setUp : function() {
+      shell = TestUtil.createShellByProtocol( "w2" );
+      shell.open();
+      shell.setBorder( null );
+      shell.setLocation( 10, 20 );
+      widget = new rwt.widgets.Composite();
+      widget.setParent( shell );
+      widget.setLocation( 10, 20 );
+      widget.setDimension( 100, 30 );
+      dropdown = new rwt.dropdown.DropDown( widget );
+      popup = dropdown._.popup;
+      TestUtil.flush();
+    },
+
+    tearDown : function() {
+      shell.destroy();
+      dropdown.destroy();
+    },
+
+    testConstructor_AddsPopupToDocument : function() {
+      assertIdentical( rwt.widgets.base.ClientDocument.getInstance(), popup.getParent() );
+    },
+
+    testConstructor_SetsDefaultPopUpStyling : function() {
+      assertEquals( "solid", popup.getBorder().getStyle() );
+      assertEquals( "#000000", popup.getBorder().getColor() );
+      assertEquals( [ 1, 1, 1, 1] , popup.getBorder().getWidths() );
+      assertEquals( "#ffffff", popup.getBackgroundColor() );
+    },
+
+    testConstructor_DoesNotMakePopUpVisible : function() {
+      TestUtil.flush();
+      assertFalse( popup.isSeeable() );
+    },
+
+    testShow_MakesPopUpVisible : function() {
+      dropdown.show();
+      TestUtil.flush();
+
+      assertTrue( popup.isSeeable() );
+    },
+
+    testHide_MakesPopUpInvisible : function() {
+      dropdown.show();
+      TestUtil.flush();
+
+      dropdown.hide();
+      TestUtil.flush();
+
+      assertFalse( popup.isSeeable() );
+    },
+
+    testShow_PositionsPopUp : function() {
+      dropdown.show();
+      TestUtil.flush();
+
+      assertEquals( 20, popup.getLeft() );
+      assertEquals( 70, popup.getTop() );
+    },
+
+    testShow_SetsPopUpWidth : function() {
+      dropdown.show();
+      TestUtil.flush();
+
+      assertEquals( 100, popup.getWidth() );
+    },
+
+    testDetroy_DisposesDropDown : function() {
+      dropdown.destroy();
+
+      assertTrue( dropdown.isDisposed() );
+    },
+
+    testDetroy_DisposesPopup : function() {
+      dropdown.destroy();
+      TestUtil.flush();
+
+      assertTrue( popup.isDisposed() );
+    },
+
+    testDetroy_ClearsReferences : function() {
+      var privateObj = dropdown._;
+      dropdown.destroy();
+
+      assertTrue( TestUtil.hasNoObjects( dropdown, true ) );
+      assertTrue( TestUtil.hasNoObjects( privateObj ) );
     }
 
   }
--- org.eclipse.rap.addon.dropdown/js/rwt/dropdown/DropDown.js
+++ org.eclipse.rap.addon.dropdown/js/rwt/dropdown/DropDown.js
@@ -14,7 +14,61 @@
 
   rwt.dropdown = {};
 
-  rwt.dropdown.DropDown = function() {
+  rwt.dropdown.DropDown = function( linkedControl ) {
+    this._ = {};
+    this._.popup = createPopup();
+    this._.linkedControl = linkedControl;
   };
+
+  rwt.dropdown.DropDown.prototype = {
+
+    show : function() {
+      checkDisposed( this );
+      var yOffset = this._.linkedControl.getHeight();
+      this._.popup.positionRelativeTo( this._.linkedControl, 0, yOffset );
+      this._.popup.setWidth( this._.linkedControl.getWidth() );
+      this._.popup.show();
+    },
+
+    hide : function() {
+      checkDisposed( this );
+      this._.popup.hide();
+    },
+
+    destroy : function() {
+      if( !this.isDisposed() ) {
+        this._.popup.destroy();
+        for( var key in this._ ) {
+          this._[ key ] = null;
+        }
+        this._ = null;
+      }
+    },
+
+    isDisposed : function() {
+      return this._ === null;
+    }
+
+  };
+
+  var createPopup = function() {
+    var result = new rwt.widgets.base.Popup();
+    result.addToDocument();
+    result.setBorder( defaultBorder );
+    result.setBackgroundColor( "#ffffff" );
+    // just for testing:
+    result.setHeight( 50 );
+    result.setRestrictToPageOnOpen( false );
+    return result;
+  };
+
+  var checkDisposed = function( dropdown ) {
+    if( dropdown.isDisposed() ) {
+      throw new Error( "DropDown is disposed" );
+    }
+  };
+
+  var defaultBorder = new rwt.html.Border( 1, "solid", "#000000" );
+
 
 }());
